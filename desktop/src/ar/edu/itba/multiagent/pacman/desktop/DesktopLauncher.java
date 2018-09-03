1dfebdeae7e5f5fb95d00af1655d289a9dc05ec6
Lucas <lcasagrande@itba.edu.ar>
2018-09-03 10:53:22 -0300

    first

---
+++ desktop/src/ar/edu/itba/multiagent/pacman/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ar.edu.itba.multiagent.pacman.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ar.edu.itba.multiagent.pacman.MultiagentPacman;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MultiagentPacman(), config);
+	}
+}
